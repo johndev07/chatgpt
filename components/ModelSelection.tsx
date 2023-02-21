1fb47992f6ce21a19fe24a184a53923fefebcd01
johndev07 <alwynjohn.dev@gmail.com>
2023-02-21 19:09:54 +0530

    model section typecript error 2 solved

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -2,17 +2,23 @@
 import React from "react";
 import useSWR from "swr";
 import Select from "react-select";
+import { ActionMeta, Options } from "react-select/dist/declarations/src";
 
 const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
-type props = {
-  value: string;
+interface OptionType {
   label: string;
-};
+  value: string;
+}
 const ModelSelection = () => {
   const { data: models, isLoading } = useSWR("models", fetchModels);
   const { data: model, mutate: setModal } = useSWR("model", {
     fallbackData: "text-davinci-003",
   });
+
+  const handleSelectChange = (option: OptionType | null) => {
+    setModal(option?.value);
+  };
+
   return (
     <div className="mt-2">
       <Select
@@ -26,7 +32,8 @@ const ModelSelection = () => {
         classNames={{
           control: (state) => "bg-[#434654] border-[#434654]",
         }}
-        onChange={(e) => setModal(e.value)}
+        onChange={handleSelectChange}
+        // onChange={(e: props) => setModal(e.value)}
       />
     </div>
   );
